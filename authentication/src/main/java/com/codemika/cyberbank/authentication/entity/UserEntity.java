b7a9837754d50b67d088b59626735ee96ee63d6a
Svarog <kau.ninja916@gmail.com>
2023-03-19 21:11:16 +0300

    Добавил UserEntity.

---
+++ authentication/src/main/java/com/codemika/cyberbank/authentication/entity/UserEntity.java
@@ -0,0 +1,38 @@
+package com.codemika.cyberbank.authentication.entity;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import javax.persistence.*;
+
+/**
+ * Entity для пользователя
+ */
+@Entity
+@Table(schema = "cyberbank_auth", name = "user")
+@Data
+@Accessors(chain = true)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    /**
+     * Используется для входа, как логин
+     */
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
