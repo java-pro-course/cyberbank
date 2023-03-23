5f42defbe6b1487f00d7f2f5f096ce877bdf97ff
chewedmole <chewedmole.git@gmail.com>
2023-03-23 23:12:58 +0700

    [kazakov] added syntax

--- authentication/src/main/java/com/codemika/cyberbank/authentication/dto/RqCreateUser.java
+++ authentication/src/main/java/com/codemika/cyberbank/authentication/dto/RqCreateUser.java
@@ -1,12 +1,15 @@
-package com.cyber.bank.dto;
+package com.codemika.cyberbank.authentication.dto;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+
 @Data
 @Accessors(chain = true)
 public class RqCreateUser {
     private String name;
     private String surname;
+    private String patronymic;
+    private String phone;
     private String email;
     private String password;
 }
