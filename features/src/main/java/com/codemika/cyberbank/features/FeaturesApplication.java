f7eed6a74dfa4978c258287c2e0b74f3df363c34
Svarog <kau.ninja916@gmail.com>
2023-03-26 20:26:41 +0300

    Очень надеюсь, что это всё заработает.

---
+++ features/src/main/java/com/codemika/cyberbank/features/FeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.codemika.cyberbank.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturesApplication.class, args);
+	}
+
+}
