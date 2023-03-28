758be9e3b53be909734879d98ef5c7c65b5284ba
Svarog <kau.ninja916@gmail.com>
2023-03-28 12:37:39 +0300

    Исправил ошибку. Теперь прога запускается успешно.

--- authentication/src/main/java/com/codemika/cyberbank/authentication/api/TestController.java
+++ authentication/src/main/java/com/codemika/cyberbank/authentication/api/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "api/auth/") // перед всеми контроллерами этого метода будет ставится этот префикс!
+@RequestMapping(value = "api/auth/") // перед всеми контроллерами этого метода будет ставиться этот префикс!
 public class TestController {
 
     @GetMapping("test") // полный url: http://localhost:8080/api/auth/test
