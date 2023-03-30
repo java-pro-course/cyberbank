accc66898738d93c68360ebc18d00166d0742bcf
chewedmole <chewedmole.git@gmail.com>
2023-03-30 22:21:13 +0700

    [kazakov] comments

--- authentication/src/main/java/com/codemika/cyberbank/authentication/api/TestController.java
+++ authentication/src/main/java/com/codemika/cyberbank/authentication/api/TestController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * Просто тестовый контроллер
+ * Контроллер для тестирования
  */
 @Data
 @RestController
