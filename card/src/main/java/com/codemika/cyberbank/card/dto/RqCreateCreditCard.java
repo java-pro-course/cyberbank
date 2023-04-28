2ab882daefda8b045554f05d0241fe8bce0a68c8
Svarog <kau.ninja916@gmail.com>
2023-04-28 21:33:27 +0300

    Доки и по-мелочи

--- card/src/main/java/com/codemika/cyberbank/card/dto/RqCreateCreditCard.java
+++ card/src/main/java/com/codemika/cyberbank/card/dto/RqCreateCreditCard.java
@@ -6,9 +6,9 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class RqCreateCreditCard {
-    private Long value;
+    private Long value; // количество требуемых денег
     private Long monthlyIncome; //ежемесячный доход для расчета платежеспособности
-    private int creditTerm; // В месяцах
+    private int creditTerm; // срок кредита в месяцах
     private String title;
     private String pincode;
 
